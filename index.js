f64d11dfbe8bf1d73e5423213bdeb212f155ee7d
Pruthvi Kumar <pruthvikumar1818@gmail.com>
2021-06-26 13:34:20 +0530

    first

---
+++ index.js
@@ -0,0 +1,43 @@
+var buttonColours = ["red","blue","green","yellow"];
+var gamePattern = [];
+var userClickedPattern=[];
+var started = false;
+var level=0;
+$(document).keypress(function(){
+		if(!started){
+			$("#level-title").text("level "+ level);
+			nextSequence();
+			started = true;
+		}
+	})
+$(".btn").click(function(){
+	var userChosenColour = $(this).attr("id");
+	userClickedPattern.push(userChosenColour);
+	playSound(userChosenColour);
+	animatePress(userChosenColour);
+});
+
+function nextSequence() {
+	level++;
+	$("#level-title").text("level "+ level);
+	var randomNumber = Math.floor(Math.random()*3+1);
+	var randomChosenColour = buttonColours[randomNumber];
+	gamePattern.push(randomChosenColour);
+$("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+playSound(randomChosenColour);
+}
+
+function playSound(name){
+	var audio = new Audio("sounds/"+name+".mp3");
+	audio.play();
+}
+
+function animatePress(currentColour){
+	$("#"+currentColour).addClass("pressed");
+	setTimeout(function(){
+	$("#"+currentColour).removeClass("pressed");
+	},100);
+}	
+$("level-title").addEventListener(keypress,function(){
+
+})
